ab730907b851bcea9aac3230bed324fb349562aa
Fraser Dillon <fraserdillon050388@gmail.com>
2021-12-01 15:27:44 +0000

    MVP Complete

---
+++ models/order.py
@@ -0,0 +1,7 @@
+class Order():
+
+    def __init__(self, customer_name, order_date, quantity, squirrel_name):
+        self.customer_name = customer_name
+        self.order_date = order_date
+        self.quantity = quantity
+        self.squirrel_name = squirrel_name
---
+++ models/order_list.py
@@ -0,0 +1,7 @@
+from models.order import *
+
+order1 = Order("Mr I Heart Squirrels", "01/11/2021", 3, "Rex")
+order2 = Order("Mr I Heart Squirrels More", "05/11/2021", 2, "I Hate Nuts")
+order3 = Order("Mr I Heart Squirrels Less", "1011/2021", 1, "I Love Nuts")
+orders = [order1, order2, order3]
+
